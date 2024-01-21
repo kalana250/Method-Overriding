30f8e9b62461596d2023845a6a330aa597a9b6ec
kalana250 <heshankalana168@gmail.com>
2024-01-21 22:40:08 +0530

    hi

---
+++ src/Company.java
@@ -0,0 +1,34 @@
+public class Company {
+
+    int id;
+    String name;
+
+
+    Company(int id, String name){
+
+        this.id = id;
+        this.name = name;
+    }
+}
+class Virtusa extends Company {
+
+    double salary;
+
+    Virtusa(int id,String name,double salary){
+        super(id, name);
+        this.salary = salary;
+    }
+
+
+
+    public void printData(){
+        System.out.println("id :- "+id+" Name :- " +name+ " salary :- "+salary);
+    }
+}
+
+class Test6{
+    public static void main(String[] args) {
+        Virtusa v = new Virtusa(43526,"Gawesh",0000.00);
+        v.printData();
+    }
+}
